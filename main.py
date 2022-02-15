ae959e0475d2d9782ddc04612ec2176832a87282
Jamie Allan <jamiealexanderallan@outlook.com>
2022-02-15 17:48:16 +0000

    Add parser to strip HTML

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from crawler import PolicyCrawler, Crawler
-
+from html_parsing import strip_tags
 
 crawler = Crawler()
 crawler.crawl(PolicyCrawler)
 
-for o in crawler.output:
-    print(o[:5000], "\n")
+with open('temp_file.txt', 'w', encoding="utf-8") as f:
+    for o in crawler.output:
+        f.write(strip_tags(o) + "\n")
