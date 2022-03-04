cc6d421682dc77b3facc7ca2a218834b9037b801
Adam Bystran <bys.adam@gmail.com>
2022-03-04 17:18:08 +0000

    Removes redundant code

--- matchrules/XpathCookieRule.py
+++ matchrules/XpathCookieRule.py
@@ -3,18 +3,11 @@ import scrapy
 
 class XpathCookieRule(MatchRuleInterface.MatchRuleInterface):
     def extract(self, response: scrapy.http.Response):
-        key = response.url
+
         items = []
-        
-        if key=="https://www.microsoft.com":
-            items.append(response.xpath("//p[contains(.//text(), 'cookie')]").get()[:1350])
-        elif key=="https://www.support.google.com":
-            items.append(response.xpath("//span[contains(.//text(), 'cookie')]").get()[:1000])
-        else:
-            items.append(response.xpath("//span[contains(., 'cookie')]/..").get())
-            items.append(response.xpath("//p[contains(., 'cookie')]/..").get())
-            items.append(response.xpath("//div[contains(., 'cookie')]/..").get())
-        pass
+        items.append(response.xpath("//span[contains(., 'cookie')]/..").get())
+        items.append(response.xpath("//p[contains(., 'cookie')]/..").get())
+        items.append(response.xpath("//div[contains(., 'cookie')]/..").get())
         
         res = ""
         
