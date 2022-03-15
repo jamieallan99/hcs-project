7659819b2b3be93d673d5f7c3cd79e4c7841e19b
luciacangarova <luciacangarova13@gmail.com>
2022-03-15 13:54:22 +0000

    main code commentary

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-# load data from csv file
+"""
+    function to load domain names from a csv file
+    return: the list of domain names
+"""
 def get_list_of_domains():
     data = pd.read_csv("top500Domains.csv")
     return data['Root Domain'].to_numpy()
