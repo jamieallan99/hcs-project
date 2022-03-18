8cc315412e2bcf8dc4e6f09aa2548af9089d4c03
Jamie Allan <jamiealexanderallan@outlook.com>
2022-03-18 15:00:10 +0000

    Finalise class for screenshot scraper

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -6,41 +6,32 @@ from selenium.webdriver.chrome.options import Options
 
 class SeleniumScreenshotter:
 
-    def __init__(self):
+    def __init__(self, headless=False):
         options = Options()
-        # options.headless = True
-        options.add_argument("--incognito")
+        options.headless = headless  # Can be turned on if just taking the screenshots
+        options.add_argument("--incognito")  # To ensure cookies aren't saved
         self.driver = webdriver.Chrome("./chromedriver.exe", options=options)
         self.driver.set_window_position(0, 0)
         self.driver.set_window_size(1024, 1024)
 
     def take_screenshot(self, url: str) -> None:
         self.driver.get(url)
-        domain = url.strip('htps:/w.').split('.')[0]
+        domain = url.split(':')[1].strip('/').replace('.', '_')
 
+        # Trys to find an element containing "cookie" hopefully this will be a banner
         try:
             element = WebDriverWait(self.driver, 3).until(
                 EC.presence_of_element_located((By.XPATH, "//*[contains(., 'cookie')]"))
             )
         except Exception:
             print(f"Couldn't find banner for {url}")
-            return
 
         try:
             self.driver.save_screenshot(f"images/{domain}_page.png")
             #element.screenshot(f"{domain}_banner.png")
         except Exception:
             print(f"Couldn't save for {url}")
-    
+        return domain
+
     def quit(self):
         self.driver.quit()
-
-
-urls = ['https://google.com', 'https://blogger.com', 'https://facebook.com']
-
-s = SeleniumScreenshotter()
-
-for url in urls:
-    s.take_screenshot(url)
-
-s.quit()
