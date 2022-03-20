706f42819f7e5e42739a3fd71c6ba52a38b9a4f7
Jamie Allan <jamiealexanderallan@outlook.com>
2022-03-20 17:50:43 +0000

    Update main to work with command line changes

--- main.py
+++ main.py
@@ -26,28 +26,17 @@ from html_parsing import strip_tags
 crawler = Crawler()
 crawler.crawl(PolicyCrawler)
 
-if args.filename is not None:
-    with open(args.filename, 'w', encoding="utf-8") as f:
+if args.f is not None:
+    with open(args.f, 'w', encoding="utf-8") as f:
         # crawl a comma separated list of urls provided by the user
-        if args.urls is not None:
-            for key, value in args.urls:
-                write_out = strip_tags(value) if value else "None"
-                f.write(key + '\n' + write_out + "\n\n")
-        else:
-            print(crawler.output.items())
-            for key, value in crawler.output.items():
-                write_out = strip_tags(value) if value else "None"
-                f.write(key + '\n' + write_out + "\n\n")
+        print(crawler.output.items())
+        for key, value in crawler.output.items():
+            write_out = strip_tags(value) if value else "None"
+            f.write(key + '\n' + write_out + "\n\n")
 else:
     with open('temp_file.txt', 'w', encoding="utf-8") as f:
-        if args.urls is not None:
-            # crawl a comma separated list of urls provided by the user
-            for key, value in args.urls:
-                write_out = strip_tags(value) if value else "None"
-                f.write(key + '\n' + write_out + "\n\n")
-        else:
-            print(crawler.output.items())
-            for key, value in crawler.output.items():
-                write_out = strip_tags(value) if value else "None"
-                f.write(key + '\n' + write_out + "\n\n")
+        print(crawler.output.items())
+        for key, value in crawler.output.items():
+            write_out = strip_tags(value) if value else "None"
+            f.write(key + '\n' + write_out + "\n\n")
 
